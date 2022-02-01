540a98df109ac721416b94c81800af9d789d2620
Soumith06 <97511845+Soumith06@users.noreply.github.com>
2022-02-01 12:00:56 +0530

    Delete UserTable/UserTable/UserTable directory

--- UserTable/UserTable/UserTable/src/main/java/net/UserData/UserTable/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package net.UserData.UserTable.Repository;
-
-import net.UserData.UserTable.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface UserRepository extends JpaRepository<User,Long> {
-
-}
